3019ca0e7d057b1a07404aefcce35e712fca5a17
bisho <girgisbishoy6@gmail.com>
2023-06-05 17:27:10 +0300

    firebase

--- lib/shared_components/theme/network/my_database.dart
+++ lib/shared_components/theme/network/my_database.dart
@@ -2,22 +2,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:todo_app/models/task_model.dart';
 
 class MyDatabase {
-
-  static CollectionReference<TaskModel> getCollection(){
-    return FirebaseFirestore.instance.collection("Task").withConverter<TaskModel>(
-      fromFirestore: (snapshot, _) => TaskModel.fromFireStore(snapshot.data()!),
-      toFirestore: (taskModel, options) => taskModel.toFireStore(),
-    );
+  static CollectionReference<TaskModel> getCollection() {
+    return FirebaseFirestore.instance
+        .collection("Task")
+        .withConverter<TaskModel>(
+          fromFirestore: (snapshot, _) =>
+              TaskModel.fromFireStore(snapshot.data()!),
+          toFirestore: (taskModel, options) => taskModel.toFireStore(),
+        );
   }
+
   static Future<void> addTask(TaskModel taskModel) {
-    var collectionRef= getCollection();
-    var doc =collectionRef.doc();
-    taskModel.id=doc.id;
+    var collectionRef = getCollection();
+    var doc = collectionRef.doc();
+    taskModel.id = doc.id;
     return collectionRef.add(taskModel);
   }
-  static Future<QuerySnapshot<TaskModel>> getTask(){
-    var collection=getCollection();
+
+  static Future<QuerySnapshot<TaskModel>> getTask() {
+    var collection = getCollection();
     return collection.get();
   }
-
 }
